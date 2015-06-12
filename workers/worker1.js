2628d1cbd08029b598e67a46f11e5d1379f4490a
ikorovictor <ikoro.victor@gmail.com>
2015-06-12 17:26:19 +0100

    Initial commit

--- workers/worker1.js
+++ workers/worker1.js
@@ -17,50 +17,47 @@ var services = require('../services')
 
 var db = null;
 
-var start = function ()
-{
+var start = function () {
 
     db.createCollection("days", function (err, bet_days) {
         if (!err) {
             var cursor = bet_days.find();
-            cursor.toArray(function (err, documents) //TODO Don't use 'toArray().length' find a better method to get item count
-            {
-                if (documents.length > 0) {
 
-                    db.createCollection('games',
+            db.createCollection('games',
 
-                        /*{short_date: val.short_date },
-                         { $set: {games: val.games, categories: val.categories}},*/
+                /*{short_date: val.short_date },
+                 { $set: {games: val.games, categories: val.categories}},*/
 
-                        function (er2, games) {
-                            if (!er2) {
-                                console.log('[DOC COUNT]: '+  documents.length )
+                function (er2, games) {
+                    if (!er2) {
+                        cursor.toArray(function (err, documents) //TODO Don't use 'toArray().length' find a better method to get item count
+                        {
+                            if (documents.length > 0) {
 
-                                async.eachSeries(documents, function( val, callback) {
+
+                                async.each(documents, function (val, callback) {
 
 
                                     services.startNoQueueBetParsingService(constants.winners_golden_bet_home, bet1_obj, bet1, val, games);
                                     services.startNoQueueBetParsingService(constants.stakersden_home, bet2_obj, bet2, val, games);
-
+                                    console.log('[DOCUMENT COUNT]: ' + documents.length)
                                 })
 
 
-                             }
+                            }
                         });
 
-                }
-            });
+                    }
+                });
         }
     });
-
 
 
 }
 
 
-MongoClient.connect(constants.MONGO_DB_URL, function(err, temp_db) {
-    if(!err)
-    {
+MongoClient.connect(constants.MONGO_DB_URL, function (err, temp_db) {
+    if (!err) {
         console.log("Connected correctly to server");
         GLOBAL.db_conn_status = 1;
         db = temp_db;
@@ -69,9 +66,8 @@ MongoClient.connect(constants.MONGO_DB_URL, function(err, temp_db) {
     }
     else {
         GLOBAL.db_conn_status = 0;
-        console.log('DB ERROR: ' +  err);
+        console.log('DB ERROR: ' + err);
     }
-
 
 
 });
