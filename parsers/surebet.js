9899f46fd7706ba920bf26c64e7ce8cbd760f22b
ikorovictor <ikoro.victor@gmail.com>
2015-06-19 13:13:43 +0100

    Dev commit

--- parsers/surebet.js
+++ parsers/surebet.js
@@ -927,7 +927,7 @@ SurebetParser.prototype.getGameOdds = function ($, game, db) {
                 else
                 {
                     console.log('[DB SAVED] GAME-ID: '+ game.id +' COUNT: ' + count );
-                    console.log('[DATA]: ' + JSON.stringify(temp_data));
+                    //console.log('[DATA]: ' + JSON.stringify(temp_data));
 
                 }
                 temp_data = null;
