839b7ab2e3472b2c009eb6ef87b8a6006c929ff3
ikorovictor <ikoro.victor@gmail.com>
2015-06-22 14:35:17 +0100

    Dev commit

--- workers/worker.js
+++ workers/worker.js
@@ -31,6 +31,7 @@ var start = function () {
                 /*{short_date: val.short_date },
                  { $set: {games: val.games, categories: val.categories}},*/
 
+
                 function (er2, games) {
                     if (!er2) {
                         cursor.toArray(function (err, documents) //TODO Don't use 'toArray().length' find a better method to get item count
@@ -50,26 +51,32 @@ var start = function () {
                                                 var bet1 = require('../parsers/nairabet').getNairabetParser();
                                                 var bet1_obj = require('../betobjects/nairabet').getNairabetObject();
                                                 services.startNoQueueBetParsingServiceSeries(constants.nairabet_home, bet1_obj, bet1, documents,0, games);
+                                                break;
                                             case 2:
                                                 var bet1 = require('../parsers/merrybet').getMerrybetParser();
                                                 var bet1_obj = require('../betobjects/merrybet').getMerrybetObject();
                                                 services.startNoQueueBetParsingServiceSeries(constants.merrybet_home, bet1_obj, bet1, documents,0, games);
+                                                break;
                                             case 3:
                                                 var bet1 = require('../parsers/surebet').getSurebetParser();
                                                 var bet1_obj = require('../betobjects/surebet').getSurebetObject();
                                                 services.startNoQueueBetParsingServiceSeries(constants.surebet_home, bet1_obj, bet1, documents,0, games);
+                                                break;
                                             case 4:
                                                 var bet1 = require('../parsers/_1960bet').get_1960betParser();
                                                 var bet1_obj = require('../betobjects/_1960bet').get_1960betObject();
                                                 services.startNoQueueBetParsingServiceSeries(constants._1960bet_home, bet1_obj, bet1, documents,0, games);
+                                                break;
                                             case 5:
                                                 var bet1 = require('../parsers/stakersden').getStakersdenParser();
                                                 var bet1_obj = require('../betobjects/stakersden').getStakersdenObject();
                                                 services.startNoQueueBetParsingServiceSeries(constants.stakersden_home, bet1_obj, bet1, documents,0, games);
+                                                break;
                                             case 6:
                                                 var bet1 = require('../parsers/winnersgoldenbet').getWGBParser();
                                                 var bet1_obj = require('../betobjects/winnersgoldenbet').getWGBObject();
                                                 services.startNoQueueBetParsingServiceSeries(constants.winners_golden_bet_home, bet1_obj, bet1, documents,0, games);
+                                                break;
 
                                         }
                                     })
