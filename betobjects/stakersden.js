046e830b3a53e758fe7c1fc7e6597d7783a2d9a4
ikorovictor <ikoro.victor@gmail.com>
2015-07-18 11:39:30 +0100

    Dev commit

--- betobjects/stakersden.js
+++ betobjects/stakersden.js
@@ -61,10 +61,12 @@ Stakersden.prototype.parse_op_with_keys = function( cheerio_object, root)
     var odds = [];
     var keys = []
 
+    console.log('here niggas');
+
     root('#outcomePanel',this).each(function(i2,e2)
     {
 
-        console.log('here niggas');
+        
         root('#outcomeText',this).each( function(){
 
             keys.push(root(this).text().trim());
