af2a74d471e5c860370ae72763d4cf70b5e63ae3
ikorovictor <ikoro.victor@gmail.com>
2015-07-18 20:13:52 +0100

    Dev commit

--- parsers/nairabet.js
+++ parsers/nairabet.js
@@ -1547,7 +1547,7 @@ NairabetParser.prototype.getGameOdds = function ($, game, db) {
         db.update(query, {$set: temp_data},
             function (err, count, status) {
                 if (err)
-                    console.log(game.id + " : " + err);
+                    console.log(game.url + " : " + err);
                 else
                 {
                     //console.log('[DB SAVED] STATUS: '+ status+' COUNT: ' + count );
